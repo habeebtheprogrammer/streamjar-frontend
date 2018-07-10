787f4002302e2141efc1ab74ea330f1f4d0dd0d0
habeebtheprogrammer <habibamil31@yahoo.com>
2018-07-10 12:41:31 +0100

    new update

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 // require("babel-core/register")
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+// var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
